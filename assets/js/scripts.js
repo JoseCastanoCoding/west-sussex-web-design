8cc1e60e486cc0044f723991c985b8c2238af338
JoseCastanoCoding <j.albertocast@gmail.com>
2022-12-23 13:57:36 +0000

    Revert "intro cards completed"
    This reverts commit c0ac1dbc61f61d1a96fc1a35eac677e18191953e.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,31 +1,16 @@
-var h1MessageArray = ["West Sussex Web Design"];
-var h2MessageArray = ["Your down-to-earth website designer"];
+var messageArray = ["Your down-to-earth website designer :)"];
 var speed = 100;
-var textPosition1 = 0;
-var textPosition2 = 0;
+var textPosition = 0;
 
 
+typewriter = () => {
+    document.querySelector("#message").innerHTML = messageArray[0].substring(0, 
+        textPosition) + '<span>\u25AE</span>';
 
-typewriter1 = () => {
-    document.querySelector("#h1Message").innerHTML = h1MessageArray[0].substring(0, 
-        textPosition1) + '<span>\u25AE</span>';
-
-    if (textPosition1++ != h1MessageArray[0].length) {
-        setTimeout("typewriter1()", speed);
+    if (textPosition++ != messageArray[0].length) {
+        setTimeout("typewriter()", speed);
     }
     
 }
 
-window.addEventListener('load', typewriter1);
-
-typewriter2 = () => {
-    document.querySelector("#h2Message").innerHTML = h2MessageArray[0].substring(0, 
-        textPosition2) + '<span>\u25AE</span>';
-
-    if (textPosition2++ != h2MessageArray[0].length) {
-        setTimeout("typewriter2()", speed);
-    }
-    
-}
-
-window.setTimeout(typewriter2, 4000);
+window.addEventListener('load', typewriter);
