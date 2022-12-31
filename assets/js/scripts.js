a147f190b079ea40e2c2a353adee4cd28687d53f
JoseCastanoCoding <j.albertocast@gmail.com>
2022-12-31 02:01:10 +0000

    all cards completed. back totop button included

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,4 +32,25 @@ var myCards = document.getElementById("intro-cards");
 
 setTimeout(() => {
     myCards.style.visibility = 'visible';
-  }, 9000);
+}, 9000);
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
